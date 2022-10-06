f94574c449b8edc8052a7d8cde748866d28cda0a
Ahmed Nadar <ahmedfay@hotmail.com>
2022-10-06 01:03:37 -0400

    Send emails to applicants

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,8 +1,14 @@
 class Email < ApplicationRecord
+  after_create_commit :send_email
+
   has_rich_text :body
 
   belongs_to :applicant
   belongs_to :user
 
   validates_presence_of :subject
+
+  def send_email
+    ApplicantMailer.contact(email: self).deliver_later
+  end
 end
