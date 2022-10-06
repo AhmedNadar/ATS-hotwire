8bc107fccef779685d68a39ae19d879a0acd1e50
Ahmed Nadar <ahmedfay@hotmail.com>
2022-10-06 00:43:18 -0400

    Build email resource

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,6 @@ Rails.application.routes.draw do
   resources :applicants do
     patch :change_stage, on: :member
     get :resume, action: :show, controller: 'resumes'
+    resources :emails, only: %i[index new create show]
   end
 end
